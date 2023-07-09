5769d563b531146ec9ccc5c9bccd3d2047ca19e8
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-09 22:05:30 +0800

    added pagination,kaminari, display the image and add references to comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  categories = ['COVID', 'Earthquake', 'Requirements']
+
+  categories.each do |category|
+  category = Category.find_or_create_by(name: category) 
+end
+ 10.times do
+    user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
+    puts "create user id: #{user.id}, email: #{user.email}"
+  end
+
+ 25.times do |i|
+    puts "start create #{i} post"
+    post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: User.all.sample, categories: Category.all.sample(1), address: Faker::Address.full_address)
+    (1..20).to_a.sample.times do
+      Comment.create(content: Faker::Lorem.sentence, user: User.all.sample, post: post)
+    end
+    puts "finish #{i} post"
+    post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: User.all.sample, address: Faker::Address.full_address)
+  end
