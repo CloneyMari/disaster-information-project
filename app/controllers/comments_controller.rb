9bef439ba9205cd1cc3b4d192b41fe58acf46900
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-09 22:08:50 +0800

    added authenticate and method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!, except: :index
     before_action :set_post
     before_action :set_comment, only: [:edit, :update, :destroy]
+    before_action :validate_comment_owner, only: [:edit, :update, :destroy]
   
     def index
-      @comments = @post.comments
+      @comments = @post.comments.includes(:user).page(params[:page]).per(5)
     end
 
     def new
@@ -39,12 +41,18 @@ class CommentsController < ApplicationController
     
       
   private
+
+    def validate_comment_owner
+      unless @comment.user == current_user
+        flash[:notice] = 'the comment not belongs to you'
+        redirect_to post_comments_path(@post)
+      end
+    end
    
     def set_post
       @post = Post.find params[:post_id]
     end
  
-  
     def comment_params
       params.require(:comment).permit(:content)
     end
