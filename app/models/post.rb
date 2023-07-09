6b491bd7ff84fe38e758acf8f1b19953ba59e26c
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-09 11:25:49 +0800

    added address in validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :address, presence: true
+
   belongs_to :user
+  has_many :comments
 end
